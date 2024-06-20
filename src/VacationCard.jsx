a8d9de26c0b98c9c884cb877d5c559199548b58e
Zachary Baca <zachary.baca@msn.com>
2024-06-20 08:01:01 -0500

    Add files via upload

---
+++ src/VacationCard.jsx
@@ -0,0 +1,12 @@
+import "../VacationCard.css";
+
+function VacationCard() {
+    return (
+        <div id="vacation-card-container">
+            <h1>Vacation Card</h1>
+            <h1>More Info</h1>
+        </div>
+    )
+}
+
+export default VacationCard;
