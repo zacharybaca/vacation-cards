c0db291df2879401b38d05c7575d2c7854497ce3
Zachary Baca <zachary.baca@msn.com>
2024-06-20 08:33:27 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,29 @@ import './App.css'
 import VacationCard from './components/VacationCard';
 
 function App() {
-  
+  let vacationSpots = [
+  {
+    place: "Meridian, Idaho",
+    price: 40,
+    timeToGo: "Spring"
+  },{
+    place: "Cancun",
+    price: 900,
+    timeToGo: "Winter"
+  },{
+    place: "China",    price: 1200,
+    timeToGo: "Fall"
+  },{
+    place: "Russia",
+    price: 1100,
+    timeToGo: "Summer"
+  },{
+    place: "Lebanon",
+    price: 400,
+    timeToGo: "Spring"
+  }
+]
+
 
   return (
     <>
