101826fa662c13d4f073e4032fc36e144c421b79
Zachary Baca <zachary.baca@msn.com>
2024-06-20 10:05:15 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,46 @@ import './App.css'
 import VacationCard from './components/VacationCard';
 
 function App() {
+  
   let vacationSpots = [
-  {
-    place: "Meridian, Idaho",
-    price: 40,
-    timeToGo: "Spring"
-  },{
-    place: "Cancun",
-    price: 900,
-    timeToGo: "Winter"
-  },{
-    place: "China",    price: 1200,
-    timeToGo: "Fall"
-  },{
-    place: "Russia",
-    price: 1100,
-    timeToGo: "Summer"
-  },{
-    place: "Lebanon",
-    price: 400,
-    timeToGo: "Spring"
-  }
-]
-
+    {
+      place: "Meridian, Idaho",
+      price: 40,
+      timeToGo: "Spring"
+    },{
+      place: "Cancun",
+      price: 900,
+      timeToGo: "Winter"
+    },{
+      place: "China",    price: 1200,
+      timeToGo: "Fall"
+    },{
+      place: "Russia",
+      price: 1100,
+      timeToGo: "Summer"
+    },{
+      place: "Lebanon",
+      price: 400,
+      timeToGo: "Spring"
+    }
+  ]
+  
 
   return (
     <>
       <div id="app-container">
-        <VacationCard />
+        <ul>
+          {vacationSpots.map((spot) => {
+            console.log(spot.classList)
+            return (
+            <li><VacationCard
+                  place={spot.place}
+                  price={spot.price}
+                  timeToGo={spot.timeToGo} 
+                />
+            </li>
+          )})}
+        </ul>
       </div>
     </>
   )
